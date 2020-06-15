d5327b3d07f3ac0b11e7b5e7f85cea0fd47c23d0
L. Theodore Obonye <ltobonye@gmail.com>
2020-06-15 14:34:26 +0200

    fix test files

--- karma.conf.js
+++ karma.conf.js
@@ -4,7 +4,6 @@ module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    files: [{ pattern: 'tests/**/*.js', type: 'module' }],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
