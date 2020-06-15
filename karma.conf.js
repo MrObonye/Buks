0292e326e4245e4e1b10b9c59a1f8358b16d36e8
L. Theodore Obonye <ltobonye@gmail.com>
2020-06-15 12:24:34 +0200

    revert karma.config.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,15 +1,15 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-export default function(config) {
+module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      import('karma-jasmine'),
-      import('karma-chrome-launcher'),
-      import('karma-jasmine-html-reporter'),
-      import('karma-coverage-istanbul-reporter'),
-      import('@angular-devkit/build-angular/plugins/karma')
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
