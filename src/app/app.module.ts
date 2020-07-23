503900344d7c2c1df0640575bb6f104551ec79e0
L. Theodore Obonye <ltobonye@gmail.com>
2020-07-23 15:33:03 +0200

    refactor: add a sticky footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { MainComponent, HeaderComponent, FooterComponent   } from './shared/index';
 
 @NgModule({
   declarations: [
-    AppComponent,
     MainComponent,
     FooterComponent,
     HeaderComponent
